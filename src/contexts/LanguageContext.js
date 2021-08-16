ca4ab7c5eb77d151f8cfa5f9b0d2a7958f4b9398
francis mwas <mwangif184@gmail.com>
2021-08-16 08:59:11 +0300

    Refactored to add hooks

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,33 +1,52 @@
-import React, { Component, createContext } from 'react';
+// Context with class based components
+// import React, { Component, createContext } from 'react';
+
+// export const LanguageContext = createContext();
+
+// export class LanguageContextProvider extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { language: 'french' };
+//     this.onLanguageChange = this.onLanguageChange.bind(this);
+//   }
+//   onLanguageChange(e) {
+//     this.setState({
+//       language: e.target.value,
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <LanguageContext.Provider
+//         value={{ ...this.state, onLanguageChange: this.onLanguageChange }}
+//       >
+//         {this.props.children}
+//       </LanguageContext.Provider>
+//     );
+//   }
+// }
+
+// export const withLanguageContext = (Component) => (props) =>
+//   (
+//     <LanguageContext.Consumer>
+//       {(value) => <Component languageContext={value} {...props} />}
+//     </LanguageContext.Consumer>
+//   );
+
+// with hooks
+
+import React, { createContext, useState } from 'react';
 
 export const LanguageContext = createContext();
 
-export class LanguageContextProvider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { language: 'french' };
-    this.onLanguageChange = this.onLanguageChange.bind(this);
-  }
-  onLanguageChange(e) {
-    this.setState({
-      language: e.target.value,
-    });
-  }
+export function LanguageContextProvider(props) {
+  const [language, setLanguage] = useState('french');
 
-  render() {
-    return (
-      <LanguageContext.Provider
-        value={{ ...this.state, onLanguageChange: this.onLanguageChange }}
-      >
-        {this.props.children}
-      </LanguageContext.Provider>
-    );
-  }
-}
+  const onLanguageChange = (e) => setLanguage(e.target.value);
 
-export const withLanguageContext = (Component) => (props) =>
-  (
-    <LanguageContext.Consumer>
-      {(value) => <Component languageContext={value} {...props} />}
-    </LanguageContext.Consumer>
+  return (
+    <LanguageContext.Provider value={{ language, onLanguageChange }}>
+      {props.children}
+    </LanguageContext.Provider>
   );
+}
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,51 +1,19 @@
-import React, { Component, createContext } from 'react';
+import React, { createContext } from 'react';
+import useToggleState from '../hooks/useToggleState';
 
 export const ThemeContext = createContext();
 
-const userData = [
-  {
-    id: 1,
-    name: 'francis',
-    email: 'mwas@gmail.com',
-    job: 'Java developer',
-  },
-  {
-    id: 1,
-    name: 'John doe',
-    email: 'doe@gmail.com',
-    job: 'Frontend developer',
-  },
+export function ThemeProvider(props) {
+  const [isDarkMode, toggleTheme] = useToggleState(false);
 
-  {
-    id: 1,
-    name: 'Jane doe',
-    email: 'jane@gmail.com',
-    job: 'Fullstack developer',
-  },
-];
-
-export class ThemeProvider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isDarkMode: true };
-
-    this.toggleTheme = this.toggleTheme.bind(this);
-  }
-
-  toggleTheme() {
-    this.setState({ isDarkMode: !this.state.isDarkMode });
-  }
-
-  render() {
-    return (
-      <ThemeContext.Provider
-        value={{
-          ...this.state,
-          toggleTheme: this.toggleTheme,
-        }}
-      >
-        {this.props.children}
-      </ThemeContext.Provider>
-    );
-  }
+  return (
+    <ThemeContext.Provider
+      value={{
+        isDarkMode,
+        toggleTheme,
+      }}
+    >
+      {props.children}
+    </ThemeContext.Provider>
+  );
 }
